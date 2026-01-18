37a47bade1f5e737f1932130e57809a5264532ab
Pablito0403 <pa0403.pablo@gmail.com>
2026-01-18 11:58:50 -0300

    Agregados los cakes redondos, falta cambiarle las imagenes para que cada uno sea diferente

--- db/config.ts
+++ db/config.ts
@@ -2,12 +2,22 @@ import { defineTable, column, defineDb } from 'astro:db';
 
 const Categories = defineTable({
 	columns: {
-		id: column.number({ primaryKey: true }),
-		label: column.text({ unique: true })
+		id: column.number({primaryKey: true}),
+		label: column.text({unique: true})
+	}
+});
+
+const Products = defineTable({
+	columns: {
+		id: column.number({primaryKey: true}),
+		name: column.text(),
+		price: column.number(),
+		category: column.number({references: ()=> Categories.columns.id}),
+		others: column.json()
 	}
 });
 
 // https://astro.build/db/config
 export default defineDb({
-  tables: {Categories}
+  tables: {Categories, Products}
 });
--- db/seed.ts
+++ db/seed.ts
@@ -1,12 +1,92 @@
-import { db, Categories } from 'astro:db';
+import { db, Categories, Products } from 'astro:db';
 
 // https://astro.build/db/seed
 export default async function seed() {
 	await db.insert(Categories).values([
-		{ id: 1, label: 'redondos' },
-		{ id: 2, label: '2 pisos' },
-		{ id: 3, label: 'cuadrados' },
-		{ id: 4, label: '3 leches' },
-		{ id: 5, label: 'Bufet' }
+		{id: 1, label: 'redondos'},
+		{id: 2, label: '2 pisos'},
+		{id: 3, label: 'cuadrados'},
+		{id: 4, label: '3 leches'},
+		{id: 5, label: 'Bufet'}
+	]);
+
+	const filling = ['coco', 'mani', 'crema de chocolate', 'crema de mantequilla'];
+
+	await db.insert(Products).values([
+		// rounded cakes
+		{
+			id: 1,
+			name: 'Mini Cake',
+			price: 3_500,
+			category: 1,
+			others: JSON.stringify({
+				size: {label: '18x10', height: 18, width: 10},
+				serves: '8',
+				filling,
+				additions: [
+					{label: 'Almibar de leche condensada', price: 600},
+					{label: 'Relleno de apollo de tiramisú (150gr)', price: 700},
+					{label: 'Relleno de apollo de frambuesa (150gr)', price: 700},
+					{label: 'Relleno de natilla', price: 500},
+					{label: 'Merengue, masa, cobertura y relleno de chocolate', price: 1_000},
+					{label: 'Almendras laminadas', price: 1_000}
+				]
+			})
+		},
+		{
+			id: 2,
+			name: 'SemiMini Cake',
+			price: 4_500,
+			category: 1,
+			others: JSON.stringify({
+				size: {label: '20x12', height: 20, width: 12},
+				serves: '10-15',
+				filling,
+				additions: [
+					{label: 'Relleno de apollo de tiramisú (200gr)', price: 1_000},
+					{label: 'Relleno de apollo de frambuesa (200gr)', price: 1_000},
+					{label: 'Merengue, masa, cobertura y relleno de chocolate', price: 1_000},
+					{label: 'Almendras laminadas', price: 1_000}
+				]
+			})
+		},
+		{
+			id: 3,
+			name: 'Cake',
+			price: 5_500,
+			category: 1,
+			others: JSON.stringify({
+				size: {label: '22x12', height: 22, width: 12},
+				serves: '15-20',
+				filling,
+				additions: [
+					{label: 'Almibar de leche condensada', price: 1_000},
+					{label: 'Relleno de apollo de tiramisú (200gr)', price: 1_500},
+					{label: 'Relleno de apollo de frambuesa (200gr)', price: 1_500},
+					{label: 'Relleno de natilla', price: 700},
+					{label: 'Merengue, masa, cobertura y relleno de chocolate', price: 1_500},
+					{label: 'Almendras laminadas', price: 1_000}
+				]
+			})
+		},
+		{
+			id: 4,
+			name: 'Super Cake',
+			price: 7_500,
+			category: 1,
+			others: JSON.stringify({
+				size: {label: '27x13', height: 27, width: 13},
+				serves: '25-30',
+				filling,
+				additions: [
+					{label: 'Almibar de leche condensada', price: 1_000},
+					{label: 'Relleno de apollo de tiramisú (200gr)', price: 1_500},
+					{label: 'Relleno de apollo de frambuesa (200gr)', price: 1_500},
+					{label: 'Relleno de natilla', price: 700},
+					{label: 'Merengue, masa, cobertura y relleno de chocolate', price: 1_500},
+					{label: 'Almendras laminadas', price: 1_500}
+				]
+			})
+		}
 	]);
 }
