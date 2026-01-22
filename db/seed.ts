697fba6497c058f7a20cc61759c23de69be9b17c
Pablito0403 <pa0403.pablo@gmail.com>
2026-01-22 11:35:23 -0300

    Agregados pasteles y comienzo del sistema de personalizacion

--- db/seed.ts
+++ db/seed.ts
@@ -87,6 +87,84 @@ export default async function seed() {
 					{label: 'Almendras laminadas', price: 1_500}
 				]
 			})
+		},
+		// two floors cakes
+		{
+			id: 5,
+			name: 'Doble Cake',
+			price: 12_000,
+			category: 2,
+			others: JSON.stringify({
+				size: {label: ['25x10', '17x10'], height: [25, 17], width: [10, 10]},
+				serves: '32-38',
+				filling,
+				additions: [
+					{label: 'Almibar de leche condensada', price: 1_000},
+					{label: 'Relleno de apollo de tiramisú (200gr)', price: 1_500},
+					{label: 'Relleno de apollo de frambuesa (200gr)', price: 1_500},
+					{label: 'Relleno de natilla', price: 700},
+					{label: 'Merengue, masa, cobertura y relleno de chocolate', price: 1_500},
+					{label: 'Almendras laminadas', price: 1_500}
+				]
+			})
+		},
+		{
+			id: 6,
+			name: 'Doble+ Cake',
+			price: 15_000,
+			category: 2,
+			others: JSON.stringify({
+				size: {label: ['30x12', '17x12'], height: [30, 17], width: [12, 12]},
+				serves: '37-43',
+				filling,
+				additions: [
+					{label: 'Almibar de leche condensada', price: 1_000},
+					{label: 'Relleno de apollo de tiramisú (200gr)', price: 1_500},
+					{label: 'Relleno de apollo de frambuesa (200gr)', price: 1_500},
+					{label: 'Relleno de natilla', price: 700},
+					{label: 'Merengue, masa, cobertura y relleno de chocolate', price: 1_500},
+					{label: 'Almendras laminadas', price: 1_500}
+				]
+			})
+		},
+		// squares cakes
+		{
+			id: 7,
+			name: 'Cake Bandeja',
+			price: 15_000,
+			category: 3,
+			others: JSON.stringify({
+				size: {label: '35x12', height: 35, width: 12},
+				serves: '40',
+				filling,
+				additions: [
+					{label: 'Almibar de leche condensada', price: 1_000},
+					{label: 'Relleno de apollo de tiramisú (200gr)', price: 1_500},
+					{label: 'Relleno de apollo de frambuesa (200gr)', price: 1_500},
+					{label: 'Relleno de natilla', price: 700},
+					{label: 'Merengue, masa, cobertura y relleno de chocolate', price: 1_500},
+					{label: 'Almendras laminadas', price: 1_500}
+				]
+			})
+		},
+		{
+			id: 8,
+			name: 'Cake Cuadrado',
+			price: 12_000,
+			category: 3,
+			others: JSON.stringify({
+				size: {label: '30x12', height: 35, width: 12},
+				serves: '30',
+				filling,
+				additions: [
+					{label: 'Almibar de leche condensada', price: 1_000},
+					{label: 'Relleno de apollo de tiramisú (200gr)', price: 1_500},
+					{label: 'Relleno de apollo de frambuesa (200gr)', price: 1_500},
+					{label: 'Relleno de natilla', price: 700},
+					{label: 'Merengue, masa, cobertura y relleno de chocolate', price: 1_500},
+					{label: 'Almendras laminadas', price: 1_500}
+				]
+			})
 		}
 	]);
 }
